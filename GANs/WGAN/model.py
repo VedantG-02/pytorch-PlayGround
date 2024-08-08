9b234c8d9257127395afc17f20bec5833f5072ab
unknown <vedantgulve2302@gmail.com>
2024-08-08 09:14:12 +0530

    wgan src added;

---
+++ GANs/WGAN/model.py
@@ -0,0 +1,104 @@
+import torch
+import torch.nn as nn
+from torchsummary import summary
+
+
+# # generator and discriminator classes below; both FCNs
+class Generator(nn.Module):
+    def __init__(self, z_dim=100, img_c=3):
+        '''
+            Args:
+            - z_dim (int) : random vector (noise) dim
+            - img_c (int) : number of image channels produced by gen
+        '''
+        super(Generator, self).__init__()
+        self.gen = nn.Sequential(
+            convBlock(in_c=z_dim, out_c=1024, disc=False, use_bn=True, use_act=True, kernel_size=4, stride=1, padding=0),
+            convBlock(in_c=1024, out_c=512, disc=False, use_bn=True, use_act=True, kernel_size=4, stride=2, padding=1),
+            convBlock(in_c=512, out_c=256, disc=False, use_bn=True, use_act=True, kernel_size=4, stride=2, padding=1),
+            convBlock(in_c=256, out_c=128, disc=False, use_bn=True, use_act=True, kernel_size=4, stride=2, padding=1),
+            convBlock(in_c=128, out_c=img_c, disc=False, use_bn=False, use_act=False, kernel_size=4, stride=2, padding=1),
+            nn.Tanh()
+        )
+
+    def forward(self, x):
+        out = self.gen(x)
+        return out
+
+class Discriminator(nn.Module):
+    def __init__(self, in_c=3):
+        '''
+            Args:
+            - in_c (int) : number of input image channels to disc
+        '''
+        super(Discriminator, self).__init__()
+        self.disc = nn.Sequential(
+            convBlock(in_c=in_c, out_c=64, disc=True, use_bn=False, use_act=True, kernel_size=4, stride=2, padding=1),
+            convBlock(in_c=64, out_c=128, disc=True, use_bn=True, use_act=True, kernel_size=4, stride=2, padding=1),
+            convBlock(in_c=128, out_c=256, disc=True, use_bn=True, use_act=True, kernel_size=4, stride=2, padding=1),
+            convBlock(in_c=256, out_c=512, disc=True, use_bn=True, use_act=True, kernel_size=4, stride=2, padding=1),
+            convBlock(in_c=512, out_c=1, disc=True, use_bn=False, use_act=False, kernel_size=4, stride=1, padding=0),
+            nn.Flatten(),
+            # nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        out = self.disc(x)
+        return out
+
+class convBlock(nn.Module):
+    def __init__(self, in_c, out_c, disc=False, use_bn=True, use_act=True, **kwargs):
+        '''
+            Args:
+            - in_c (int) : number of input channels to conv layer
+            - out_c (int) : number of output channels from conv layer
+            - disc (bool) : flag to use convBlock in gen or disc (activation changes)
+            - use_bn (bool) : flag to identify if bn is to be used
+            - use_act (bool) : flag to identify if activation is to be used
+        '''
+        super(convBlock, self).__init__()
+        self.use_bn = use_bn
+        self.use_act = use_act
+        self.conv = nn.ConvTranspose2d(in_c, out_c, bias=not use_bn, **kwargs) if disc==False else nn.Conv2d(in_c, out_c, bias=not use_bn,**kwargs)
+        self.bn = nn.BatchNorm2d(out_c)
+        self.act = nn.ReLU() if disc==False else nn.LeakyReLU(0.2)
+    
+    def forward(self, x):
+        out = self.conv(x)
+        out = self.bn(out) if self.use_bn else out
+        out = self.act(out) if self.use_act else out
+        return out
+    
+def initialize_weights(model):
+    '''
+        All weights are initialized from a normal distribution with mean=0 and std=0.02.
+    '''
+    for m in model.modules():
+        if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d, nn.BatchNorm2d)):
+            nn.init.normal_(m.weight.data, 0.0, 0.02)
+
+
+def test():
+    bs = 16
+    img_c = 1
+    z_dim = 100
+
+    x_disc = torch.randn((bs, img_c, 64, 64)).cuda()
+    x_gen = torch.rand((bs, z_dim, 1, 1)).cuda()  # uniform distribution
+    gen = Generator(z_dim=z_dim, img_c=img_c).cuda()
+    disc = Discriminator(in_c=img_c).cuda()
+
+    out_gen = gen(x_gen)
+    out_disc = disc(x_disc)
+
+    assert out_gen.shape == (bs, img_c, 64, 64), "generator output shape doesn't match"
+    assert out_disc.shape == (bs, 1), "discriminator output shape doesn't match"
+
+    summary(gen, (z_dim, 1, 1))
+    summary(disc, (img_c, 64, 64))
+
+    print("\n# ---Testing Done--- #\n")
+
+
+if __name__ == '__main__':
+    test()
---
+++ GANs/WGAN/train.py
@@ -0,0 +1,100 @@
+import torch
+import torchvision
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from torch.utils.tensorboard import SummaryWriter
+from model import Generator, Discriminator, initialize_weights
+import os
+
+
+# torch.manual_seed(42) # reproducibility purpose
+
+
+# constants
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+Z_DIM = 100
+IMG_SIZE = 64
+
+
+# dataset and dataloader
+transforms_ = transforms.Compose([
+    transforms.Resize((IMG_SIZE, IMG_SIZE)),
+    transforms.ToTensor(),  
+    transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+])
+dataset = datasets.ImageFolder(root="data/celeb_faces_data/", transform=transforms_)
+dataloader = DataLoader(dataset, batch_size=64, shuffle=True)
+
+
+# model, optim initialization
+critic = Discriminator(in_c=3).to(DEVICE)
+gen = Generator(z_dim=Z_DIM, img_c=3).to(DEVICE)
+initialize_weights(gen)
+initialize_weights(critic)
+opt_critic = optim.RMSprop(critic.parameters(), lr=0.00005)
+opt_gen = optim.RMSprop(gen.parameters(), lr=0.00005)
+
+
+# fixed noise to see results of while training generator (on tensorboard)
+fixed_noise = torch.randn((64, Z_DIM, 1, 1)).to(DEVICE)
+
+
+# tensorboard writers
+BOARD_PATH = "runs"
+writer_fake = SummaryWriter(f"WGAN/tensorboard/{BOARD_PATH}/fake")
+writer_real = SummaryWriter(f"WGAN/tensorboard/{BOARD_PATH}/real")
+step = 0
+
+
+# training
+critic.train()
+gen.train()
+print("\nTraining is starting...\n")
+for epoch in range(30):
+    for batch_id, (real, _) in enumerate(dataloader):
+        # print(batch_id+1)
+        real = real.to(DEVICE)
+        batch_size = real.shape[0]
+
+        # train critic : max(E(f(real)) - E(f(fake)))
+        for i in range(5): # n_critic=5; not experimenting with this
+            noise = torch.randn(batch_size, Z_DIM, 1, 1).to(DEVICE)
+            fake = gen(noise)
+            critic_real = critic(real).view(-1)
+            critic_fake = critic(fake.detach()).view(-1)
+            loss_critic = -1*(torch.mean(critic_real) - torch.mean(critic_fake))
+            critic.zero_grad()
+            loss_critic.backward()
+            opt_critic.step()
+            for param in critic.parameters():
+                param.data.clamp_(-0.01, 0.01)
+
+        # train generator : max(E(f(fake)))
+        output = critic(fake).view(-1)
+        loss_gen = -1*torch.mean(output)
+        gen.zero_grad()
+        loss_gen.backward()
+        opt_gen.step()
+
+        if (batch_id+1) % (len(dataloader)//2) == 0:
+            print(f"Epoch [{epoch+1:>2}/30], Batch {batch_id+1}/{len(dataloader)}, Loss D: {loss_critic:.5f}, Loss G: {loss_gen:.5f}")
+
+    # log at each epoch end
+    with torch.no_grad():
+        fake = gen(fixed_noise)
+        img_grid_fake = torchvision.utils.make_grid(fake, normalize=True)
+        img_grid_real = torchvision.utils.make_grid(real, normalize=True)
+
+        writer_fake.add_image("CELEB Fake Images", img_grid_fake, global_step=step)
+        writer_real.add_image("CELEB Real Images", img_grid_real, global_step=step)
+        
+        step += 1
+    print()
+
+if not os.path.isdir(os.path.join('WGAN', 'models')):
+    os.mkdir(os.path.join('WGAN', 'models'))
+torch.save(gen, 'WGAN/models/generator.pth')
+torch.save(critic, 'WGAN/models/criticriminator.pth')
